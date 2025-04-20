109cb3f00f673fb4bd21f75bb1207a63c3953948
Zack Low <lowzack@gmail.com>
2025-04-20 13:33:22 -0400

    Update Employment

--- data/employment.ts
+++ data/employment.ts
@@ -1,44 +1,62 @@
 export default {
   currentEmployment: {
-    company: "Clover Network, Inc.",
-    logo: "/static/clover_primary_gray_rgb.webp",
-    logoAlt: "Clover Network, Inc. Logo",
-    website: "https://www.clover.com/",
-    dates: "October 2018 - present",
-    overview:
-      "Lead a highly efficient global team of cross-functional engineers through the software development lifecycle of multiple consumer facing web services and applications.",
+    company: "GlossGenius",
+    logo: "",
+    logoAlt: "GlossGenius Logo",
+    website: "https://glossgenius.com/",
+    dates: "2023 - present",
+    overview: "Current role details to be added.",
     roles: [
       {
-        title: "Engineering Manager",
-        dates: "August 2021 - present",
+        title: "Current Role Title",
+        dates: "2023 - present",
         highlights: [
-          "Crafted recruitment and hiring processes focused on candidate quality, efficiency of developer time, and clear communication. Eventually, this process was used to hire 80+ offshore engineering resources and is currently in use by 3 onshore teams.",
-          "Communicated with product, engineering, and design stakeholders to scope and plan projects to scale the company's consumer facing ordering, loyalty and eCommerce web and mobile applications.",
-          "Conducted performance reviews, goal setting, salary reviews and resource planning in order to manage, develop, and retain talent.",
-        ],
-      },
-      {
-        title: "Senior Software Engineer, Tech Lead",
-        dates: "March 2020 - August 2022",
-        highlights: [
-          "Mentored other engineers to advance their skills and career using a goal-based approach to evaluate consistent progress, with multiple being promoted throughout the process.",
-          "Spearheaded an engineering team that built and maintained a performant, highly available, and multi-locale first-party online ordering product for SMBs that integrates directly with the Clover POS using Java, Spring Boot, Redis, MySQL and React/Typescript.",
-          "Pioneered real-time application performance monitoring to create a proactive culture around bugs and issues using DataDog, PagerDuty and Wavefront to monitor errors and the performance of Spring Boot microservices. Datadog was then later accepted as our APM and Logging SaaS provider ",
-          "Championed service decomposition efforts with knowledge shares, technical documentation and contributions to a parent project providing Clover specific functionality for company developed Spring Boot microservices deployed to Google Cloud Platform (GCP) using Java, Terraform, Puppet and Jenkins.",
-        ],
-      },
-      {
-        title: "Software Engineer II",
-        dates: "October 2018 - March 2020",
-        highlights: [
-          "Implemented Scrum and eventually Scrum-of-scrum practices within the team as it scaled to better track and forecast work, developing standards for processes, estimation, and ceremonies within the team.",
-          "Lead the development of cross-cutting features with multiple teams and financial business units in order to increase new merchant approval percentage through Clover’s self-service application product.",
-          "Revamped lead generation and management from being part of a monolithic architecture to a microservices architecture, advancing service decomposition efforts by utilizing modern technologies, domain knowledge and API design. Key technologies include Java, Spring Boot, Hibernate/JPA, and Redis Pub/Sub and GCP.",
+          "Highlight 1",
+          "Highlight 2",
         ],
       },
     ],
   },
   previousEmployment: [
+    {
+      company: "Clover Network, Inc.",
+      logo: "/static/clover_primary_gray_rgb.webp",
+      logoAlt: "Clover Network, Inc. Logo",
+      website: "https://www.clover.com/",
+      dates: "October 2018 - 2023",
+      overview:
+        "Lead a highly efficient global team of cross-functional engineers through the software development lifecycle of multiple consumer facing web services and applications.",
+      roles: [
+        {
+          title: "Engineering Manager",
+          dates: "August 2021 - 2023",
+          highlights: [
+            "Crafted recruitment and hiring processes focused on candidate quality, efficiency of developer time, and clear communication. Eventually, this process was used to hire 80+ offshore engineering resources and is currently in use by 3 onshore teams.",
+            "Communicated with product, engineering, and design stakeholders to scope and plan projects to scale the company's consumer facing ordering, loyalty and eCommerce web and mobile applications.",
+            "Conducted performance reviews, goal setting, salary reviews and resource planning in order to manage, develop, and retain talent.",
+          ],
+        },
+        {
+          title: "Senior Software Engineer, Tech Lead",
+          dates: "March 2020 - August 2022",
+          highlights: [
+            "Mentored other engineers to advance their skills and career using a goal-based approach to evaluate consistent progress, with multiple being promoted throughout the process.",
+            "Spearheaded an engineering team that built and maintained a performant, highly available, and multi-locale first-party online ordering product for SMBs that integrates directly with the Clover POS using Java, Spring Boot, Redis, MySQL and React/Typescript.",
+            "Pioneered real-time application performance monitoring to create a proactive culture around bugs and issues using DataDog, PagerDuty and Wavefront to monitor errors and the performance of Spring Boot microservices. Datadog was then later accepted as our APM and Logging SaaS provider ",
+            "Championed service decomposition efforts with knowledge shares, technical documentation and contributions to a parent project providing Clover specific functionality for company developed Spring Boot microservices deployed to Google Cloud Platform (GCP) using Java, Terraform, Puppet and Jenkins.",
+          ],
+        },
+        {
+          title: "Software Engineer II",
+          dates: "October 2018 - March 2020",
+          highlights: [
+            "Implemented Scrum and eventually Scrum-of-scrum practices within the team as it scaled to better track and forecast work, developing standards for processes, estimation, and ceremonies within the team.",
+            "Lead the development of cross-cutting features with multiple teams and financial business units in order to increase new merchant approval percentage through Clover's self-service application product.",
+            "Revamped lead generation and management from being part of a monolithic architecture to a microservices architecture, advancing service decomposition efforts by utilizing modern technologies, domain knowledge and API design. Key technologies include Java, Spring Boot, Hibernate/JPA, and Redis Pub/Sub and GCP.",
+          ],
+        },
+      ],
+    },
     {
       company: "Constant Contact",
       logo: "/static/ctct-logo.svg",
@@ -74,7 +92,7 @@ export default {
           highlights: [
             "Refactored an internal ticketing and asset tracking system, moving all backend code to a REST service in PHP and MySQL, and all frontend code into a SPA using Backbone.js. This allowed new features to be developed quicker and easier, using current web technologies and best practices.",
             "Designed and developed a full-stack web application using jQuery, PHP, and MySQL to track and audit the training of IT service technicians, provide crucial reporting to upper management, and identify deficiencies in the process. This system was also rolled out to additional departments to facilitate the training of their employees.",
-            "Created a training program for additional developers to learn the languages and technologies behind the departments’ web applications to allow future development and to reduce technical debt over time.",
+            "Created a training program for additional developers to learn the languages and technologies behind the departments' web applications to allow future development and to reduce technical debt over time.",
           ],
         },
       ],
